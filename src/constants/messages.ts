bf9ebdcb6f0170b34730a03efecc62880101edba
Vordgi <sasha2822222@gmail.com>
2022-03-23 09:02:44 +0200

    Update help information

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,9 +1,9 @@
 export const HELP = `
-    '--old', '-o' {String} - old domain ("https://old-dolmain.com" or "https://old-dolmain.com/en")
-    '--new', '-n' {String} - new domain ("https://new-domain.com" or "https://new-domain.com/en")
+    '--old', '-o' {String} - old domain (f.e. "https://old-domain.com" or "https://old-domain.com/en")
+    '--new', '-n' {String} - new domain (f.e. "https://new-domain.com" or "https://new-domain.com/en")
     '--main-selector', '-m' {String} - selector to get full height (body by default)
     '--width', '-w' {Number} - browser screen width (1440 by default)
-    '--page', '-p' {String[]} - list of pages ("/about", "/contacts")
+    '--page', '-p' {String[]} - list of pages (f.e. "/about", "/contacts")
     '--await', '-a' {String[]} - expected elements selectors
     '--conceal', '-c' {String[]} - hidden elements selectors
     '--sitemap', '-s' {Boolean} - get list of pages from sitemap ({oldDomain}/sitemap.xml)
